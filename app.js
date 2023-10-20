030789fc30873cd2b42555c30a7e1ce6f611f17e
Christian Barbosa <christian.barbosa05222001@gmail.com>
2023-10-21 06:07:06 +0800

    Add some comment

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ function toggleOverlay() {
     const overlay = document.getElementById('overlay');
     overlay.classList.toggle('active');
 }
+
+window.addEventListener('scroll', function() {
+    // Calculate the scroll position
+    const scrollPosition = window.scrollY;
+
+    // Set a threshold for when you want to change the background color
+    const scrollThreshold = 200; // Change this value as needed
+
+    // Get the element where you want to change the background color
+    const contentElement = document.getElementById('content');
+
+    // Change the background color based on scroll position
+    if (scrollPosition > scrollThreshold) {
+        contentElement.style.backgroundColor = 'lightblue'; // Change to your desired color
+    } else {
+        contentElement.style.backgroundColor = ''; // Reset to the default color
+    }
+});
